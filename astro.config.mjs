c8b3cf23989643975dc95f8bbc0f360010c6367e
rootedsmile-stack <geetsupra50@gmail.com>
2025-11-28 21:58:00 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [
@@ -11,5 +11,5 @@ export default defineConfig({
     }),
   ],
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
